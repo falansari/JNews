76019c6c158161bf7fddecacf1f02ac5a4ae88be
Ash Alansari <twoseasgamesstudio@gmail.com>
2026-04-15 19:17:04 +0300

    #18 Updated send newsletter email service to send correct newsletter's contents with customized title and subscriber name #22

--- src/main/java/com/ga/JNews/services/EmailService.java
+++ src/main/java/com/ga/JNews/services/EmailService.java
@@ -1,7 +1,9 @@
 package com.ga.JNews.services;
 
+import com.ga.JNews.exceptions.BadRequestException;
 import com.ga.JNews.exceptions.InformationNotFoundException;
 import com.ga.JNews.models.Newsletter;
+import com.ga.JNews.models.Subscriber;
 import com.ga.JNews.models.enums.SubscriberStatus;
 import com.ga.JNews.repositories.EmailRepository;
 import com.ga.JNews.utilities.Uploads;
@@ -11,7 +13,10 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Objects;
 
 @Service
 public class EmailService {
@@ -33,22 +38,43 @@ public class EmailService {
         Newsletter newsletter = newsletterService.getNewsletterById(newsletterId);
         if (newsletter == null) throw new InformationNotFoundException("Newsletter with id " + newsletterId + " not found");
 
-        String[] recipients = subscriberService.getSubscribersEmailAddresses(subscriberStatus);
-        if (recipients == null) throw new InformationNotFoundException("There are no subscribers.");
+        String newsletterHtml;
+        String newsletterText;
 
-        try {
-            MimeMessage message = mailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+        try { // Retrieve newsletter's HTML and plain text bodies from server
+            newsletterHtml = Objects.requireNonNull(uploads.downloadFile(newsletterService.uploadPathBodyHtml,
+                    newsletter.getBodyHtml()).getBody()).getContentAsString(StandardCharsets.UTF_8)
+                    .replace("{{title}}", newsletter.getTitle()); // Replace newsletter's {{title}}
+            newsletterText = Objects.requireNonNull(uploads.downloadFile(newsletterService.uploadPathBodyText,
+                    newsletter.getBodyText()).getBody()).getContentAsString(StandardCharsets.UTF_8)
+                    .replace("{{title}}", newsletter.getTitle());
+        } catch (IOException e) {
+            throw new BadRequestException("Error while retrieving newsletter bodies");
+        }
 
-            helper.setTo(recipients);
-            helper.setSubject(newsletter.getSubject());
-            helper.setText(uploads.downloadFile(newsletterService.uploadPathBodyHtml, newsletter.getBodyText()).toString(),
-                    uploads.downloadFile(newsletterService.uploadPathBodyHtml, newsletter.getBodyHtml()).toString());
+        ArrayList<Subscriber> recipients = subscriberService.getSubscribersByStatus(subscriberStatus);
+        if (recipients == null) throw new InformationNotFoundException("There are no recipients");
 
-            mailSender.send(message);
-            System.out.println("Email sent successfully");
-        } catch (MessagingException e) {
-            throw new RuntimeException(e);
+        // TODO: make the operation Asynchronous
+        for (Subscriber subscriber : recipients) {
+            try {
+                // Replace newsletter's {{subscriber}} placeholder
+                String customizedHtml = newsletterHtml.replace("{{subscriber}}", subscriber.getName());
+                String customizedText = newsletterText.replace("{{subscriber}}", subscriber.getName());
+
+                MimeMessage message = mailSender.createMimeMessage();
+                MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+                helper.setTo(subscriber.getEmail());
+                helper.setSubject(newsletter.getSubject());
+                helper.setText(customizedText, customizedHtml);
+
+                mailSender.send(message);
+
+                // TODO: save sent e-mail records to db
+            } catch (MessagingException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
